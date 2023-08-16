551f21763e76af2fab7ab761bc6dbae2100bde19
FaridaFatali <faridafatali.91@gmail.com>
2023-08-16 11:55:41 +0400

    Initial commit

---
+++ src/main/java/org/example/PongGame.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Practice: https://www.youtube.com/watch?v=oLirZqJFKPE
+ * @author Farida Fatali
+ * Executable 2D Pong Game with Java
+ * To move the 1st player use W and S, for the 2nd player use Up and Down keys
+ */
+
+public class PongGame {
+    public static void main(String[] args) {
+        GameFrame frame = new GameFrame();
+    }
+}
